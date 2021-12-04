1e9a18e6878256eeb44f3c19554788eeece87160
Ian Cooper <ian.cooper@just-eat.com>
2021-12-04 11:00:24 +0100

    Advent of Code - Day one, part two

--- internal/one.go
+++ internal/one.go
@@ -1,5 +1,7 @@
 package advent_of_code
 
+import "container/list"
+
 func DepthIncreases(changes []int) int {
 	increases := 0
 	priorValue := 0
@@ -11,4 +13,55 @@ func DepthIncreases(changes []int) int {
 	}
 
 	return increases
+}
+
+func GroupedDepthIncreases(changes []int, groupSize int) int {
+	return findIncreases(sumGroups(groupReadings(changes, groupSize)))
+}
+
+func groupReadings(changes []int, groupSize int) []*list.List {
+	var groupedDepths = make([]*list.List, len(changes))
+	//populate a grouped map
+	for index, value := range changes {
+		//create a new group
+		readings := list.New()
+		groupedDepths[index] = readings
+
+		//now iterate over the groups and add to any that are not full yet
+		for _, rds := range groupedDepths {
+			if rds != nil && rds.Len() < groupSize {
+				rds.PushBack(value)
+			}
+		}
+	}
+	return groupedDepths
+}
+
+func findIncreases(summedGroup []int) int {
+	increases := 0
+	priorValue := 0
+	for _, value := range summedGroup {
+		if priorValue > 0 && value > priorValue {
+			increases++
+		}
+		priorValue = value
+	}
+
+	return increases
+}
+
+func sumGroups(groupedDepths []*list.List) []int {
+	var summedGroup = []int{}
+	for _, group := range groupedDepths {
+		if group != nil && group.Len() == 3 {
+			totalDepth := 0
+			el := group.Front()
+			for el != nil {
+				totalDepth += el.Value.(int)
+				el = el.Next()
+			}
+			summedGroup = append(summedGroup, totalDepth)
+		}
+	}
+	return summedGroup
 }
--- internal/one_test.go
+++ internal/one_test.go
@@ -10,5 +10,14 @@ func TestDepthIncrease(t *testing.T) {
 	if count != 7 {
 		t.Errorf("Failed. Count was %d and expected 7", count)
 	}
+}
 
+func TestGroupedDepthIncrease(t *testing.T) {
+
+	changes := []int{199, 200, 208, 210, 200, 207, 240, 269, 260, 263}
+
+	count := GroupedDepthIncreases(changes, 3)
+	if count != 5 {
+		t.Errorf("Failed count was %d and expected 5", count)
+	}
 }
