d3038b0f588848b463508f45954e70f316d8c251
Ian Cooper <ian.cooper@just-eat.com>
2022-12-05 13:59:51 +0000

    Passing 1_2

--- 2022/internal/one.go
+++ 2022/internal/one.go
@@ -1,6 +1,9 @@
 package advent_of_code_2022
 
-import "strconv"
+import (
+	"sort"
+	"strconv"
+)
 
 func GreatestCaloriesCounter(packs []string) int {
 
@@ -20,4 +23,41 @@ func GreatestCaloriesCounter(packs []string) int {
 	}
 
 	return highestCalories
+}
+
+func HighestCaloriesCounter(packs []string, i int) int {
+
+	var totalHighestCalories = 0
+	highestCalories := make([]int, 0, 10)
+	packCalories := 0
+	for _, ration := range packs {
+		if ration == "" {
+			highestCalories = append(highestCalories, packCalories)
+			packCalories = 0
+			continue
+		}
+		if r, err := strconv.Atoi(ration); err == nil {
+			packCalories += r
+		}
+	}
+
+	sort.Ints(highestCalories)
+	reverse(highestCalories)
+	totalHighestCalories = sum(highestCalories[:i])
+
+	return totalHighestCalories
+}
+
+func reverse(input []int) {
+	for i, j := 0, len(input)-1; i < j; i, j = i+1, j-1 {
+		input[i], input[j] = input[j], input[i]
+	}
+}
+
+func sum(array []int) int {
+	result := 0
+	for _, v := range array {
+		result += v
+	}
+	return result
 }
--- 2022/internal/one_test.go
+++ 2022/internal/one_test.go
@@ -11,3 +11,12 @@ func TestGreatestCalorieCounter(t *testing.T) {
 		t.Errorf("Failed. Calories was %d and expected 24000", greatestCalories)
 	}
 }
+
+func TestHighestThreeCalorieCounter(t *testing.T) {
+	packs := []string{"1000", "2000", "3000", "", "4000", "5000", "6000", "", "7000", "8000", "9000", "", "10000"}
+
+	highestCalories := HighestCaloriesCounter(packs, 3)
+	if highestCalories != 45000 {
+		t.Errorf("Failed. Calories was %d and expected 24000", highestCalories)
+	}
+}
